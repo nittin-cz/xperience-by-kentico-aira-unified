09559b74fdecb8f88c23f87221f1bc1aa800fe1d
Boris Kapustík <kapustik@nittin.cz>
2024-12-18 18:41:56 +0100

    fix(ACAController): correct namespace usings

--- src/Kentico.Xperience.Aira/AiraCompanionAppController.cs
+++ src/Kentico.Xperience.Aira/AiraCompanionAppController.cs
@@ -6,8 +6,6 @@ using Kentico.Xperience.Aira.Admin.InfoModels;
 using Kentico.Xperience.Aira.Chat.Models;
 using Htmx;
 
-using Kentico.Membership;
-
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +14,8 @@ using Microsoft.AspNetCore.Http;
 using Kentico.Xperience.Aira.Authentication;
 using Kentico.Xperience.Aira.Assets;
 using Kentico.Xperience.Aira.Registration;
+using Kentico.Xperience.Aira.Membership;
+using Kentico.Xperience.Aira.Services;
 
 namespace Kentico.Xperience.Aira;
 
