195d85e963af646a76f816e81082c3eda34e8326
Hess Pavel <hess@nittin.cz>
2025-07-17 10:56:09 +0200

    fix: code formatting

--- src/Kentico.Xperience.AiraUnified/Chat/MockAiHttpClient.cs
+++ src/Kentico.Xperience.AiraUnified/Chat/MockAiHttpClient.cs
@@ -71,7 +71,6 @@ internal sealed class MockAiHttpClient : IAiHttpClient
         });
 
 
-
     private Task<AiraUnifiedAIResponse?> GetOngoingMessageResponse(AiraUnifiedAIRequest request)
     {
         // Check if the message contains any insights-related keywords
